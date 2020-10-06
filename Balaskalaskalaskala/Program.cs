9939e6f51337cd3506c38fb4db9bbe93bb35f59b
JohnFury77 <furijani99@gmail.com>
2020-10-06 12:58:08 +0200

    Balaskalaskalaskalaskalaska

--- Balaskalaskalaskala/Program.cs
+++ Balaskalaskalaskala/Program.cs
@@ -11,16 +11,26 @@ namespace Balaskalaskalaskala
         public static Karakter jatekos;
         public static char Menu()
         {
-            return 'a';
+            char k = 'k';
+
+            do
+            {
+                Console.WriteLine("'a' menüpont: Harcol\n'b' menüpont: Gyógyul\n'c' menüpont: Kilépés");
+                k = char.Parse(Console.ReadLine());
+
+            } while (k!='a'&& k != 'b'&& k != 'c');
+            return char.ToLower(k);
         } 
         static void Main(string[] args)
         {
+            Console.WriteLine("Üdvözlünk Temeriában.");
             Console.WriteLine("Mi lesz a karaktered neve?");
             string karakternev = Console.ReadLine();
             Console.WriteLine("Mi lesz a karaktered kasztja? \n1: Paladin \n2: Warrior \n3: Elf");
             int karakterkaszt = int.Parse(Console.ReadLine());
-            jatekos = new Karakter(karakternev, karakterkaszt);
             
+            jatekos = new Karakter(karakternev, karakterkaszt);
+            Menu();
             //mindent megváltoztató komment
             Karakterek karakterekLista = new Karakterek("karakterek.txt");
             Kiiratas(karakterekLista);
@@ -29,8 +39,8 @@ namespace Balaskalaskalaskala
 
         public static void Kiiratas(Karakterek karakterekLista)
         {
-            Console.WriteLine("#1\t"+jatekos.ToString());
-            int i = 2;
+            
+            int i = 1;
             foreach (Karakter k in karakterekLista.KarakterLista)
             {
                 Console.WriteLine("#{0}\t"+k.ToString(),i);
