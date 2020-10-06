5bba095251538dfe48a49fcc7d3186f2179f80fa
JohnFury77 <furijani99@gmail.com>
2020-10-06 12:23:13 +0200

    Balaskalaskalaskalaska

--- Balaskalaskalaskala/Program.cs
+++ Balaskalaskalaskala/Program.cs
@@ -18,6 +18,9 @@ namespace Balaskalaskalaskala
             Console.WriteLine("Mi lesz a karaktered neve?");
             string karakternev = Console.ReadLine();
             Console.WriteLine("Mi lesz a karaktered kasztja? \n1: Paladin \n2: Warrior \n3: Elf");
+            int karakterkaszt = int.Parse(Console.ReadLine());
+            Karakter jatekos = new Karakter(karakternev, karakterkaszt);
+
             //mindent megváltoztató komment
             Karakterek karakterekLista = new Karakterek("karakterek.txt");
             Kiiratas(karakterekLista);
